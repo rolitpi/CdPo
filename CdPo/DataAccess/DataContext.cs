c24b339555e1fe9db01bcb40662f376af4244165
Даниил Цебрук <tsebrukdan@bk.ru>
2022-11-14 21:56:33 +0300

    Added EF Core

---
+++ CdPo/DataAccess/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using CdPo.Model.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace CdPo.DataAccess;
+
+public class DataContext: DbContext
+{
+    public DataContext(DbContextOptions options) : base(options) {}
+    
+    public DbSet<Person> Persons { get; set; }
+    
+    public DbSet<Staff> Staffs { get; set; }
+    
+    public DbSet<Student> Students { get; set; }
+    
+    public DbSet<Contract> Contracts { get; set; }
+    
+    public DbSet<Discipline> Disciplines { get; set; }
+    
+    public DbSet<TrainingCourse> TrainingCourses { get; set; }
+    
+    public DbSet<Group> Groups { get; set; }
+}
